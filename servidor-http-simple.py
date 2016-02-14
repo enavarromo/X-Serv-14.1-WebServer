3f280ee140d09ed1a8f8c4ebf109690a88b09132
Eloy <eloyin@ubuntu.(none)>
2016-02-14 02:31:41 +0100

    Añado mi codigo

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python
-
-"""
-Simple HTTP Server
-Jesus M. Gonzalez-Barahona and Gregorio Robles
-{jgb, grex} @ gsyc.es
-TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
-"""
-
 import socket
 
-# Create a TCP objet socket and bind it to a port
-# We bind to 'localhost', therefore only accepts connections from the
-# same machine
-# Port should be 80, but since it needs root privileges,
-# let's use one above 1024
 
+# -------------- Port Set Up --------------
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#mySocket.bind(('localhost', 1234))  # Socket LoopBack
+#mySocket.bind((socket.gethostname(), 1234))  # Socket LoopBack Host
+mySocket.bind(('192.168.1.132', 1234))  # Socket wlan0 inet addr:192.168.1.132
+mySocket.listen(2) # 5 TPC Cons cap
 
-# Queue a maximum of 5 TCP connection requests
-
-mySocket.listen(5)
-
-# Accept connections, read incoming data, and answer back an HTML page
-#  (in an infinite loop)
-
-while True:
-    print 'Waiting for connections'
-    (recvSocket, address) = mySocket.accept()
-    print 'HTTP request received:'
-    print recvSocket.recv(1024)
-    recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
-                    "\r\n")
-    recvSocket.close()
+try:
+    while True:
+        print 'Waiting for connections'
+        (recvSocket, address) = mySocket.accept()
+        # -------------- Plot Socket Info --------------
+        print 'HTTP request received from: '+ str(address)
+        print recvSocket.recv(1024)
+        if address[0] == '127.0.0.1':
+            print "Coming from Loopback"
+            # -------------- HTTP to LoopBack --------------
+            recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+                            "<html><body><h1>Hello World!</h1>" +
+                            "You came from LoopBack at IP: " +
+                            str(address[0]) + 
+                            " and Port: " + str(address[1]) +
+                            "</body></html>\r\n")
+        else:
+            # -------------- HTTP to ext --------------
+            recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+                            "<html><body><h1>Hello World!</h1>" +
+                            "You are at IP: " + str(address[0]) +
+                            " and Port: " + str(address[1]) +
+                            "</body></html>\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    mySocket.close()
+    print("\nExiting Ok")
